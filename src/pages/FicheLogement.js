3a4e83dbda108c9b7c6b159bd9706c34e9ee7e72
Florian Beau <florian.beau6942@gmail.com>
2023-02-23 16:32:31 +0100

    Css FicheLogement

--- src/pages/FicheLogement.js
+++ src/pages/FicheLogement.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import Header from "../components/Header";
-
+import "../styles/styleFicheLogement.sass";
 import Offers from "../data/data.json";
 
 const FicheLogement = () => {
@@ -14,12 +14,31 @@ const FicheLogement = () => {
   return (
     <div>
       <Header />
-      <h1>Offres de locations</h1>
-      <p>Offres du jour</p>
-      <p>{offer.title}</p>
-      <p>{offer.description}</p>
-      <p>{offer.location}</p>
-      <p>{offer.equipments}</p>
+      <div className="offerContainer">
+        <div>
+          <img src={offer.cover} alt="offer picture" className="img" />
+        </div>
+        <div>
+          <h1 className="h1">{offer.title}</h1>
+          <h2 className="localisation">{offer.location}</h2>
+          <p className="tags">{offer.tags}</p>
+          {/* Container "Description" and "Equipements": */}
+          <div className="containerFlexDiv borderStyle">
+            <div className="borderStyle">
+              <h3 className="titleFlexDiv">Description</h3>
+              <ul>
+                <p className="redText">{offer.description}</p>
+              </ul>
+            </div>
+            <div className="borderStyle">
+              <h3 className="titleFlexDiv">Equipements</h3>
+              <ul>
+                <p className="redText">{offer.equipments}</p>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
 
       <br />
     </div>
