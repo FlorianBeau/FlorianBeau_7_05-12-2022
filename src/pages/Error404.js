ea52f3d14678af325a856315551a3ea732fdb3af
Florian Beau <florian.beau6942@gmail.com>
2023-06-12 12:27:50 +0200

    Save page error 404

--- src/pages/Error404.js
+++ src/pages/Error404.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Header from "../components/Header";
+import "../styles/styleError404.scss"
 
 // Composant permettant d'afficher le composant Header, ainsi que la phrase
 // indiquant l'erreur.
@@ -9,8 +10,9 @@ const Error404 = () => {
     <div>
       <Header underlined={"about"} />
       <div>
-        <p className="title404">404</p>
-        <p>Oups! La page que vous demandez n'existe pas.</p>
+        <h1 className="title404">404</h1>
+        <h2 className="oups">Oups! La page que vous demandez n'existe pas.</h2>
+        <h3 className="backlink"><a className="link" href="/">Retourner vers la page d'accueil</a></h3>
       </div>
       <br />
     </div>
