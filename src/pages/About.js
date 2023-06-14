7532a2d5b5618b89a2f24eb00c6a8d1311b03ca2
Florian Beau <florian.beau6942@gmail.com>
2023-06-14 16:24:15 +0200

    Beggin about page

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,17 +1,82 @@
-import React from "react";
+import React, { useState } from "react";
 import Banner from "../components/Banner";
 import Header from "../components/Header";
 import BannerImage from "../assets/kalen-emsley-Bkci_8qcdvQ-unsplash 2.png";
+import "../styles/styleAbout.scss";
+import arrowDown from "../assets/arrow_down.png";
+import arrowUp from "../assets/arrow_up.png";
 
-// Composant permettant d'afficher les composant Header et Banner sur la page
-// "à propos".
 const About = () => {
+  const [isShow2, setIsShow2] = useState(false);
+
   return (
     <div>
+
       <Header underlined={"about"} />
       <Banner imageSrc={BannerImage} title="" />
-      <h1>A propos</h1>
       <br />
+
+      {isShow2 ? (
+        <div className="containerCollapse">
+            {/* Collapse une fois ouvert */}
+            <div className="divCollapseOpen">
+                <div>
+                    <h3>Fiabilité</h3>
+                </div>
+                <div>
+                    <button className="divButton" onClick={() => setIsShow2(false)}>
+                        <img src={arrowUp} alt="arrow_Up" />
+                    </button>
+                </div>
+            </div>
+            <div>
+                <p className="p_Description"></p>
+            </div>
+        </div>) : (
+          <div className="containerCollapse">
+            <div className="divCollpaseClosed">
+                <div>
+                    <h3 className="textWhite">Description</h3>
+                </div>
+                <div>
+                    <button className="divButton" onClick={() => setIsShow2(true)}>
+                        <img src={arrowDown} alt="arrow_Down" />
+                    </button>
+                </div>
+            </div>
+        </div>      )
+
+<div className="divUp">
+                <div>
+                    <h3>Description</h3>
+                </div>
+                <div>
+                    <button className="divButton" onClick={() => setIsShow(false)}>
+                        <img src={arrowUp} alt="arrow_Up" />
+                    </button>
+                </div>
+            </div>
+            <div>
+                <p className="p_Description">{offer.description}</p>
+            </div>
+        </div>
+                ) : (
+        <div>
+            <div className="divUp">
+                <div>
+                    <h3 className="textWhite">Description</h3>
+                </div>
+                <div>
+                    <button className="divButton" onClick={() => setIsShow(true)}>
+                        <img src={arrowDown} alt="arrow_Down" />
+                    </button>
+                </div>
+            </div>
+        </div>
+        )
+
+
+      }
     </div>
   );
 };
